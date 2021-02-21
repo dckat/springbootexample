4f67e0882b91d3574f30ed5a54731dcd6f733527
LG <kdc9619@naver.com>
2021-02-21 17:33:29 +0900

    게시판 글 조회,수정,삭제 구현

--- src/main/java/com/springboot/example/boot/service/PostsService.java
+++ src/main/java/com/springboot/example/boot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.springboot.example.boot.service;
 
 import com.springboot.example.boot.domain.posts.Posts;
 import com.springboot.example.boot.domain.posts.PostsRepository;
+import com.springboot.example.boot.web.dto.PostsListResponseDto;
 import com.springboot.example.boot.web.dto.PostsResponseDto;
 import com.springboot.example.boot.web.dto.PostsSaveRequestDto;
 import com.springboot.example.boot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,9 @@ import com.springboot.example.boot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,5 +41,21 @@ public class PostsService {
                         IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/springboot/example/boot/web/HelloController.java
+++ src/main/java/com/springboot/example/boot/web/HelloController.java
@@ -1,10 +1,12 @@
 package com.springboot.example.boot.web;
 
 import com.springboot.example.boot.web.dto.HelloResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class HelloController {
 
